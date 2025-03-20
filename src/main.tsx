489a061c916486be1a8acf46b561ef2d94569fd1
Bruna Gomes <brunagomes9165@gmail.com>
2025-03-20 17:01:29 -0400

    fix:configurações

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import App from './App'
 
 import './index.css'
 
-import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <div>
+      <h1>teste</h1>
+    </div>
   </React.StrictMode>,
 )
 
