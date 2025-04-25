c00bd12dbb01b398f4acfbe79d9ba094d9cba2c9
Bruna Gomes <brunagomes9165@gmail.com>
2025-04-25 16:21:57 -0400

    feat(back): logar no sistema

--- backend/router/loginRouter.php
+++ backend/router/loginRouter.php
@@ -3,8 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-require_once __DIR__ . "/../controller/userController.php";
-$userController = new UserController();
+require_once __DIR__ . "/../controller/loginController.php";
+$loginController = new LoginController();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     switch ($_GET["acao"]) {
@@ -12,21 +12,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $valores = json_decode(file_get_contents("php://input"), true);
                 $email = $valores["email"];
                 $senha = $valores["senha"];
-    
-                
-                $verificarEmail = $userController->BuscarEmail($email);
-            if ($verificarEmail) {
-                $resultado = $userController->ValidaLogin($email, $senha);
+                $senha_hash = hash("sha256", $senha);
+                $resultado = $loginController->BuscarLogin($email, $senha_hash);
 
                 if ($resultado) {
                     echo json_encode(array("status" => 200, "message" => "Login realizado com sucesso!"));
                 } else {
                     echo json_encode(array("status" => 400, "message" => "Erro ao realizar login!"));
                 }
-                break;;
-            } else {
-                echo json_encode(array("status" => 500, "message" => "Email não cadastrado!"));
-            }
+                break;
+             
         
         default:
             echo "Não achei";
